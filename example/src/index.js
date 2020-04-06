2d8a73e7f902b2fbf22e3bfa88b5f45d78494d8a
lizeng <lizeng@guanghe.tv>
2020-04-06 11:39:25 +0800

    支持函数组件

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,7 @@
 // import React, { createElement } from '../../packages/react/lib/react.js';
 // import React, { createElement } from '../../packages/react2/lib/react.js';
-import React, { createElement } from "../../packages/react2-1/lib/react.js";
+// import React, { createElement } from "../../packages/react2-1/lib/react.js";
+import React, { createElement } from "../../packages/react3/lib/react.js";
 // import React, { createElement } from '../../packages/react-fc/lib/react.js';
 
 // const App = (props) => {
@@ -18,38 +19,95 @@ import React, { createElement } from "../../packages/react2-1/lib/react.js";
 
 // React.render(<App />, document.getElementById('root'))
 
-const App = (
-  <div id="demo1">
-    <h1
-      onClick={() => {
-        reRender();
-      }}
-    >
-      #1: React is so easy!
-    </h1>
-    <p style={{ fontSize: 16, color: "#333" }}>Do you think so?</p>
-  </div>
-);
+// const App = (props) => {
+//   return (
+//     <div id="demo">
+//       <h1
+//         onClick={() => {
+//           alert(1);
+//         }}
+//       >
+//         #: React is so easy!
+//       </h1>
+//       <p style={{ fontSize: 16, color: "#333" }}>Do you think so?</p>
+//     </div>
+//   )
+// }
 
-const App2 = (
-  <div id="demo2">
-    <h2
-      onClick={() => {
-        firstRender();
-      }}
-    >
-      #2: React is so easy!
-    </h2>
-    <p style={{ fontSize: 16, color: "#333" }}>Do you think so?</p>
-  </div>
-);
+// React.render(<App />, document.getElementById("root"));
+
+// const App = (
+//   <div id="demo1">
+//     <h1
+//       onClick={() => {
+//         reRender();
+//       }}
+//     >
+//       #1: React is so easy!
+//     </h1>
+//     <p style={{ fontSize: 16, color: "#333" }}>Do you think so?</p>
+//   </div>
+// );
+
+// const App2 = (
+//   <div id="demo2">
+//     <h2
+//       onClick={() => {
+//         firstRender();
+//       }}
+//     >
+//       #2: React is so easy!
+//     </h2>
+//     <p style={{ fontSize: 16, color: "#333" }}>Do you think so?</p>
+//   </div>
+// );
+
+// const firstRender = () => {
+//   React.render(App, document.getElementById("root"));
+// };
+
+// const reRender = () => {
+//   React.render(App2, document.getElementById("root"));
+// };
+
+// firstRender();
+
+const App = props => {
+  return (
+    <div id="demo1">
+      <h1
+        onClick={() => {
+          reRender();
+        }}
+      >
+        #1: React is so easy!
+      </h1>
+      <p style={{ fontSize: 16, color: "#333" }}>Do you think so?</p>
+    </div>
+  );
+};
+
+const App2 = props => {
+  return (
+    <div id="demo2">
+      <h2
+        onClick={() => {
+          firstRender();
+        }}
+      >
+        #2: React is so easy!
+      </h2>
+      <p style={{ fontSize: 16, color: "#333" }}>Do you think so?</p>
+    </div>
+  );
+};
 
 const firstRender = () => {
-  React.render(App, document.getElementById("root"));
+  React.render(<App />, document.getElementById("root"));
 };
 
 const reRender = () => {
-  React.render(App2, document.getElementById("root"));
+  React.render(<App2 />, document.getElementById("root"));
 };
 
 firstRender();
